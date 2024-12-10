4f7b82698c567962b9ddd362a81bf17eadea8faf
cheowha1 <cheowha1@naver.com>
2024-12-10 10:44:34 +0900

    Update 04-boolean.js

--- 04-boolean.js
+++ 04-boolean.js
@@ -22,3 +22,18 @@ let b1 = a > b;  // 비교 연산의 결과
 let b2 = b >= 0 && b <= 10 ;   // 논리 연산의 결과
 // 논리 연산자 : AND(&&), OR(||), NOT(!) -> 논리 결과 조합
 console.log(b1, b2);
+
+console.log("================");
+// Boolean 타입 함수를 이용한 Boolean 판정
+// number : 값이 있으면 true, 값이 없으면 false
+// string : 내용이 있으면 true, 빈 문자열이면 false
+console.log(Boolean("ECMAScript"), Boolean(""));
+console.log(Boolean(2024), Boolean(0));
+
+// ShortCircuit
+console.log(true && "Test");
+console.log(false && "Test");
+
+console.log(true || "Test");
+
+// React의 선택적 렌더링에 많이 활용
