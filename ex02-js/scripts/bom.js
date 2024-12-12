e22086e54736bba8639e4417e2db9559b92b4d2c
cheowha1 <cheowha1@naver.com>
2024-12-12 10:58:33 +0900

    BOM 외부 스크립트 작성

---
+++ ex02-js/scripts/bom.js
@@ -0,0 +1,20 @@
+let newwin;
+
+function openWindow() {
+    // window.open("https://www.daum.net");
+    newWin = window.open("https://www.daum.net",
+                "daum",
+                "width=400, height=400, left=100, top=100"
+    );
+}
+
+function closeWindow() {
+    if (newWin !== undefined && !newWin.closed)
+        newWin.close();
+}
+
+function printInner() {
+    let message = `현재 윈도우 창의 크기는 ${window.innerWidth} x ${window.innerHeight} 입니다`;
+    alert(message);
+}
+let name = prompt("이름을 입력해 주세요");
